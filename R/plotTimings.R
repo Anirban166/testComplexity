69bf13fc970a129ded99b306c269769b0800d78f
Anirban166 <bloodraven166@gmail.com>
2020-05-13 14:16:20 +0530

    Update plotTimings()

--- R/plotTimings.R
+++ R/plotTimings.R
@@ -1,4 +1,6 @@
 plotTimings = function(data.frame = df)
 {
- ggplot(df, aes(x=datasetsize, y=time)) + geom_point(aes(color = expr)) + geom_line(aes(color = expr)) + labs(x="N", y="Runtime") + scale_x_log10() + scale_y_log10()
+
+ ggplot(df, aes(x = `Data set sizes`, y = Timings)) + geom_point() + geom_line() + labs(x = "N", y = "Runtime") + scale_x_log10() + scale_y_log10()
+
 }
