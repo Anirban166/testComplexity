471c52367310e8e307612cfa59e8106754696fd9
Anirban166 <bloodraven166@gmail.com>
2020-05-23 14:26:48 +0530

    Update time complexity classifying function

--- R/asymptoticTimeComplexityClass.R
+++ R/asymptoticTimeComplexityClass.R
@@ -1,12 +1,12 @@
-asymptoticTimeComplexityClass = function(to.model)
+asymptoticTimeComplexityClass = function(model.df)
 {
-  constant   <- glm(Timings~1,                                      data = to.model); to.model['constant'] = fitted(constant)
-  linear     <- glm(Timings~`Data set sizes`,                       data = to.model); to.model['linear'] = fitted(linear)
-  squareroot <- glm(Timings~sqrt(`Data set sizes`),                 data = to.model); to.model['squareroot'] = fitted(squareroot)
-  log        <- glm(Timings~log(`Data set sizes`),                  data = to.model); to.model['log'] = fitted(log)
-  log.linear <- glm(Timings~`Data set sizes`*log(`Data set sizes`), data = to.model); to.model['log-linear'] = fitted(log.linear)
-  quadratic  <- glm(Timings~I(`Data set sizes`^2),                  data = to.model); to.model['quadratic'] = fitted(quadratic)
-  cubic      <- glm(Timings~I(`Data set sizes`^3),                  data = to.model); to.model['cubic'] = fitted(cubic)
+  constant   <- glm(Timings~1,                                      data = model.df); model.df['constant'] = fitted(constant)
+  linear     <- glm(Timings~`Data set sizes`,                       data = model.df); model.df['linear'] = fitted(linear)
+  squareroot <- glm(Timings~sqrt(`Data set sizes`),                 data = model.df); model.df['squareroot'] = fitted(squareroot)
+  log        <- glm(Timings~log(`Data set sizes`),                  data = model.df); model.df['log'] = fitted(log)
+  log.linear <- glm(Timings~`Data set sizes`*log(`Data set sizes`), data = model.df); model.df['log-linear'] = fitted(log.linear)
+  quadratic  <- glm(Timings~I(`Data set sizes`^2),                  data = model.df); model.df['quadratic'] = fitted(quadratic)
+  cubic      <- glm(Timings~I(`Data set sizes`^3),                  data = model.df); model.df['cubic'] = fitted(cubic)
 
   model.list <- list('constant'   = constant,
                      'linear'     = linear,
@@ -17,7 +17,7 @@ asymptoticTimeComplexityClass = function(to.model)
                      'cubic'      = cubic
                     )
 
-  cross.validated.errors <- lapply(model.list, function(x) cv.glm(to.model, x)$delta[2])
+  cross.validated.errors <- lapply(model.list, function(x) cv.glm(model.df, x)$delta[2])
   best.model <- names(which.min(cross.validated.errors))
   print(best.model)
 }
