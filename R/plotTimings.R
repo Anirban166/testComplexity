671d55b242f8610088bdbc1c1aaa9dafdafb75f5
Anirban166 <bloodraven166@gmail.com>
2020-06-20 22:15:32 +0530

    Add (color) and update (alpha) optional parameters for plotters

--- R/plotTimings.R
+++ R/plotTimings.R
@@ -7,23 +7,30 @@
 #' @param titles A list of two elements, consisting of the plot title and subtitle.
 #' # Optional, with default values set to empty strings. (no titles/subtitles)
 #'
-#' @param alpha.value A value denoting transparency level (in the range 0 to 1) for point and line geometry.
+#' @param point.alpha A numeric value denoting transparency level (in the range 0 to 1) for point geometry.
 #' # Optional, with the default value set to 1. (no transparentness)
+#'
+#' @param line.alpha A numeric value denoting transparency level (in the range 0 to 1) for line geometry.
+#' # Optional, with the default value set to 1. (no transparentness)
+#'
+#' @param point.color A string specifying a known color or a representation in hexcode for point geometry.
+#' # Optional, with the default color set as black. (Hex equivalent: #000000)
+#'
+#' @param line.color A string specifying a known color or a representation in hexcode for line geometry.
+#' # Optional, with the default color set as black. (Hex equivalent: #000000)
 #'
 #' @return A ggplot object.
 #'
 #' @export
 #' @import ggplot2
 
-plotTimings = function(data.df, titles = list("",""), alpha.value)
+plotTimings = function(data.df, titles = list("",""), point.alpha = 1, line.alpha = 1, point.color = "black", line.color = "black")
 {
  if(class(data.df) == "data.frame" & "Timings" %in% colnames(data.df) & "Data sizes" %in% colnames(data.df))
  {
-   alpha.value = ifelse(missing(alpha.value), 1, alpha.value)
-
    plot.obj <- ggplot(data.df, aes(x = `Data sizes`, y = Timings)) +
-               geom_point(alpha = alpha.value) +
-               geom_line(alpha = alpha.value) +
+               geom_point(alpha = point.alpha, color = point.color) +
+               geom_line(alpha = line.alpha, color = line.color) +
                labs(x = "Data sizes", y = "Runtime (in nanoseconds)") +
                scale_x_log10() + scale_y_log10() +
                ggtitle(titles[[1]], subtitle = titles[[2]])
