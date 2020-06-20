6689e2dcc7d0086e03b85a4ced8756bd5df164b6
Anirban166 <bloodraven166@gmail.com>
2020-06-20 22:55:41 +0530

    Add optional geom side + labels

--- R/plotMemoryUsage.R
+++ R/plotMemoryUsage.R
@@ -4,8 +4,10 @@
 #'
 #' @param data.df A data frame composed of columns 'Memory Usage' and 'Data sizes', which can be obtained by asymptoticMemoryUsage()
 #'
-#' @param titles A list of two elements, consisting of the plot title and subtitle.
+#' @param titles A list of two elements consisting of strings for the plot title and subtitle.
 #' # Optional, with default values set to empty strings. (no titles/subtitles)
+#'
+#' @param labels A list of two elements containing strings for x and y labels respectively.
 #'
 #' @param point.alpha A numeric value denoting transparency level (in the range 0 to 1) for point geometry.
 #' # Optional, with the default value set to 1. (no transparentness)
@@ -19,19 +21,25 @@
 #' @param line.color A string specifying a known color or a representation in hexcode for line geometry.
 #' # Optional, with the default color set as black. (Hex equivalent: #000000)
 #'
+#' @param point.size A numeric value denoting the size of point geometry.
+#' # Optional, with the default value set to (1.3).
+#'
+#' @param point.size A numeric value denoting the line geometry.
+#' # Optional, with the default value set to (0.7).
+#'
 #' @return A ggplot object.
 #'
 #' @export
 #' @import ggplot2
 
-plotMemoryUsage = function(data.df, titles = list("",""), point.alpha = 1, line.alpha = 1, point.color = "black", line.color = "black")
+plotMemoryUsage = function(data.df, titles = list("", ""), labels = list("Data sizes", "Memory size (in bytes)"), point.alpha = 1, line.alpha = 1, point.color = "black", line.color = "black", point.size = 1.3, line.size = 0.7)
 {
   if(class(data.df) == "data.frame" & "Memory usage" %in% colnames(data.df) & "Data sizes" %in% colnames(data.df))
   {
     plot.obj <- ggplot(data.df, aes(x = `Data sizes`, y = `Memory usage`)) +
-                geom_point(alpha = point.alpha, color = point.color) +
-                geom_line(alpha = line.alpha, color = line.color) +
-                labs(x = "Data sizes", y = "Memory size (in bytes)") +
+                geom_point(alpha = point.alpha, color = point.color, size = point.size) +
+                geom_line(alpha = line.alpha, color = line.color, size = line.size) +
+                labs(x = labels[[1]], y = labels[[2]]) +
                 scale_x_log10() + scale_y_log10() +
                 ggtitle(titles[[1]], subtitle = titles[[2]])
 
