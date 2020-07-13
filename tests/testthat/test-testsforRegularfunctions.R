cd5818ca3f350424b49cf7c9cdeff833767b9115
Anirban166 <bloodraven166@gmail.com>
2020-07-13 13:15:56 +0530

    Add tests for Regular functions of linear complexities (substring & gregexpr)

---
+++ tests/testthat/test-testsforRegularfunctions.R
@@ -0,0 +1,11 @@
+library(testthat)
+
+# Tests for linear time complexity sorting algorithms
+
+test_that("substring function test", {
+  expect_linear_time(asymptoticTimings, substring(paste(rep("A", data.sizes), collapse = ""), 1:data.sizes, 1:data.sizes), data.sizes = 10^seq(1, 5, by = 0.5), max.seconds = 1)
+})
+
+test_that("gregexpr function test", {
+  expect_linear_time(asymptoticTimings, gregexpr("a", paste(collapse = "", rep("ab", data.sizes)), perl = TRUE), data.sizes = 10^seq(1, 5, by = 0.5), max.seconds = 1)
+})
