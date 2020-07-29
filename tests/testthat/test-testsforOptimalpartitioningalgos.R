1ca6f9e70387421127b71f7d175bfdd84c26f29b
Anirban166 <bloodraven166@gmail.com>
2020-07-30 00:02:57 +0530

    Add test for gfpop::gfpop

--- tests/testthat/test-testsforOptimalpartitioningalgos.R
+++ tests/testthat/test-testsforOptimalpartitioningalgos.R
@@ -12,3 +12,10 @@ if(require(fpop)) {
     expect_loglinear_time(Fpop(rnorm(data.sizes), 1), data.sizes = 10^seq(1, 5, by = 0.5))
   })
 }
+
+if(require(gfpop)) {
+  test_that("gfpop::gfpop function test", {
+    # Test for gfpop::gfpop, a loglinear time functional pruning algorithm using optimal partitioning
+    expect_loglinear_time(gfpop(data = dataGenerator(as.integer(data.sizes), c(0.1, 0.2, 0.3, 0.4, 0.6, 0.8, 1), c(0, 0.5, 1, 1.5, 2, 2.5, 3), sigma = 1), mygraph = graph(penalty = 2*log(as.integer(data.sizes)), type = "isotonic"), type = "mean"), data.sizes = 10^seq(1, 4, by = 0.5))
+  })
+}
